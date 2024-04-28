17d59e50248daf5828d494ded9939cb531b8ed9f
shashwat pal <shashwatpal1021@gmail.com>
2024-04-28 11:39:06 +0530

    learning dart

---
+++ Dart/main.dart
@@ -0,0 +1,20 @@
+// stronger static and dynamic checking
+//  Idiomatic js code generation for better interoperability.
+
+void main(){
+  print("Hello World");
+}
+
+//  Indetifiers are names given to elements in aprogram like variables, functions etc. The  rules for Identifiers are as follows:
+
+// Indentifiers can include both character and digits. However, the Identifier cannot befin with a digit.
+
+// -> identifier cannot include special symbols except for underscore (_) and dollar sign ($).
+
+// -> Identifiers are case sensitive.
+
+// -> Identifiers cannot be keywords.
+
+// -> they must be unique.
+
+// -> indentifiers cannot contain spaces.
